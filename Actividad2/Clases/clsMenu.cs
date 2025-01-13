618225bfba1f7087ff4122e0e9e38e5b8c1bbf67
Carlos Ardila Romero <ar_dilacar@hotmail.com>
2025-01-12 20:13:41 -0500

    nuevo proyecto, actividad 2

---
+++ Actividad2/Clases/clsMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2.Clases
+{
+    public class clsMenu
+    {
+        public void ListarOpciones()
+        {
+            string opcion = "0";
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Bienvenidos al Curso Chalo CODE");
+                Console.WriteLine("Actividad # 2");
+                Console.WriteLine("Presentado por Carlos Ardila");
+
+                Console.WriteLine("------------------------------------------");
+
+                Console.WriteLine("  MENU DE OPCIONES:\n\n Digite el numero de la opción deseada:\n" +
+                    "\n 1-  Ingresar" +
+                    "\n 2-  Eliminar" +
+                    "\n 3-  Buscar" +
+                    "\n 4-  Ordenar" +
+                    "\n 0-  Salir");
+
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Porfavor Ingrese los valores");
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.WriteLine("Porfavor eliminar los valores");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Console.WriteLine("Porfavor que valor desea buscar");
+                        Console.ReadKey();
+                        break;
+                    case "4":
+                        Console.WriteLine("se han ordenado los valores");
+                        Console.ReadKey();
+                        break;
+                    case "0":
+                        Console.WriteLine("Gracias por utilizar nuestros servicios");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Ingresaste una opcion no valida, Intenta de nuevo");
+                        Console.ReadKey();
+                        break;
+
+                }
+            } while (opcion != "0");
+        }
+    }
+}
---
+++ Actividad2/Program.cs
@@ -0,0 +1,9 @@
+﻿     class Program
+    {
+        static void Main()
+        {
+        Actividad2.Clases.clsMenu menu = new Actividad2.Clases.clsMenu();
+        menu.ListarOpciones();
+    }
+    }
+
