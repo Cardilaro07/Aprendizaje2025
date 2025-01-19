daaf76d71b5543ed304f7a24c2f1e9645262f415
Carlos Ardila Romero <ar_dilacar@hotmail.com>
2025-01-19 10:17:59 -0500

    Contraseña logueo: 123

--- Actividad2/Program.cs
+++ Actividad2/Program.cs
@@ -2,8 +2,8 @@
 {
     static void Main()
     {
-       // Actividad2.Clases.clsLogin login = new Actividad2.Clases.clsLogin();
-      //  login.Logueo();
+       Actividad2.Clases.clsLogin login = new Actividad2.Clases.clsLogin();
+       login.Logueo();
 
         Actividad2.Clases.clsMenu menu = new Actividad2.Clases.clsMenu();
         menu.ListarOpciones();
